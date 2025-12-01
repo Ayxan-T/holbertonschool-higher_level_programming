804b29e95d39c5980b2774e7940b5a17277df692
Me <me@example.com>
2025-12-01 11:45:03 +0000

    auto commit

---
+++ python-import_modules/2-args.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def main():
+    import sys
+    if len(sys.argv) == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments.".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{}: {}".format(i, sys.argv[i])
+
+
+if __name__ == "__main__":
+    main()
