bf4f18ec05d9eca1fa50aafc54572c5159237f24
Me <me@example.com>
2025-12-01 11:54:45 +0000

    auto commit

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def main():
+    import sys
+
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+
+    print(sum)
+
+if __name__ == "__main__":
+    main()
