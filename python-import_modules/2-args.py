248dda3368e1ac17157d0ead8243451895967e59
Me <me@example.com>
2025-12-01 11:49:39 +0000

    auto commit

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,8 +5,10 @@ def main():
     import sys
     if len(sys.argv) == 2:
         print("1 argument:")
+    elif len(sys.argv) == 1:
+        print("0 arguments.")
     else:
-        print("{} arguments.".format(len(sys.argv) - 1))
+        print("{} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
 
