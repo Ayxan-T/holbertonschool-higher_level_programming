47352675b1e1f838b197d7d6fd72c9196b850f03
Me <me@example.com>
2025-12-03 13:33:12 +0000

    auto commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for element in row:
+        for element in row[:-1]:
             print("{:d}".format(element), end=" ")
-        print()
+        print("{:d}".format(row[-1]))
