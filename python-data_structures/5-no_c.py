107838c0de7bb74ab0f8b3d42bec7534dc8aec8e
Me <me@example.com>
2025-12-03 13:21:26 +0000

    auto commit

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -3,13 +3,6 @@
 
 def no_c(my_string):
     indexes = []
-    # for each char in my_string
-        # if char is c or C
-            # add its index to cC_indexes
-    # for each index in cC_indexes
-        # slice my_string in two parts, add them and assign it to my_string
-
-    # return my_string
 
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
