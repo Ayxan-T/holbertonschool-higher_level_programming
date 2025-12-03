e7140b6e7c981c8c2a9d6870476716d08d60b43e
Me <me@example.com>
2025-12-03 13:37:41 +0000

    auto commit

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,6 +2,9 @@
 # here goes your f comment
 
 def print_matrix_integer(matrix=[[]]):
+    if len(matrix[0]) == 0:
+        print()
+        exit()
     for row in matrix:
         for element in row[:-1]:
             print("{:d}".format(element), end=" ")
