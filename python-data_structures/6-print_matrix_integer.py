c96b7c4d75ead01397862a1374cde6578ca22d2c
Me <me@example.com>
2025-12-03 13:28:40 +0000

    auto commit

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# here goes your f comment
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for element in row:
+            print("{:d} ".format(element))
+        print()
