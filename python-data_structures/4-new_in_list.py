39310babf6ab7051a49f0bf5c1399a301f9bb776
Me <me@example.com>
2025-12-03 13:00:39 +0000

    auto commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,6 +2,16 @@
 # just a random comment
 
 def new_in_list(my_list, idx, element):
+    replace = False
     if idx >= 0 and idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+        replace = True
+
+    new_list = []
+    for i in range(0, len(my_list)):
+        if i == idx:
+            if replace:
+                new_list.append(element)
+        else:
+            new_list.append(my_list[i])
+
+    return new_list
