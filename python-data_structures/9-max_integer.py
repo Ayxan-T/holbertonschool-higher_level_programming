947fe468a041b1ad8165c8658222bf2b8f6f8c4a
Me <me@example.com>
2025-12-04 11:10:21 +0000

    auto commit

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,6 +2,8 @@
 # here goes your f comment
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     maxint = my_list[0]
     for i in range(1, len(my_list)):
         if my_list[i] > maxint:
