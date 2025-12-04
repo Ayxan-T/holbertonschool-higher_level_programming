cc87cff97094dd37f3d939e039f1c79386f7b49c
Me <me@example.com>
2025-12-04 11:04:48 +0000

    auto commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,7 +2,7 @@
 # here goes your f comment
 
 def multiple_returns(sentence):
-    l = len(sentence)
-    if l == 0:
-        return (l, None)
-    return (l, sentence[0])
+    length = len(sentence)
+    if length == 0:
+        return (length, None)
+    return (length, sentence[0])
