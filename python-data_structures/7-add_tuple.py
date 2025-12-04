390c2820a7ee5a79218de4867c463f3da66b3ada
Me <me@example.com>
2025-12-04 10:56:57 +0000

    auto commit

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# here goes your f comment
+
+def add_tuple(tuple_a=(), tuple_b()):
+    sums = [0, 0]
+
+    k = -1
+    for item in tuple_a:
+        if k == 1:
+            break
+        k += 1
+        sums[k] += item
+    
+    k = -1
+    for item in tuple_b:
+        if k == 1:
+            break
+        k += 1
+        sums[k] += item
