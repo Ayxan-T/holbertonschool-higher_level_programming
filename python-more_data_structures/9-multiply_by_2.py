9ea858c7f37d8106e424f576eef22b5f9002a23b
Me <me@example.com>
2025-12-05 06:39:49 +0000

    auto commit

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,6 @@
 
 def multiply_by_2(a_dictionary):
     res = dict()
-    if key in a_dictionary.keys():
+    for key in a_dictionary.keys():
         res[key] = a_dictionary[key] * 2
     return res
