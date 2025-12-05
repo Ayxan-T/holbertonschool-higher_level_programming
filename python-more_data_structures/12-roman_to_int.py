c7a53fd4e207e657df72afb83492d4340264b405
Me <me@example.com>
2025-12-05 07:16:09 +0000

    auto commit

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if !isinstance(roman_string, str) or !roman_stirng:
+        return None
+    roman_dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    res = roman_dict[roman_string[0]]
+    for i in range(1, len(roman_string)):
+        if roman_dict[roman_string[i]] > roman_dict[roman_string[i-1]]:
+            res -= roman_dict[roman_string[i-1]]*2
+        res += roman_dict[roman_string[i-1]]
+    return res
