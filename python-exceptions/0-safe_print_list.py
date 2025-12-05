6d5cba1dd8626e67f8f5564fca83e71db6a00851
Me <me@example.com>
2025-12-05 08:02:54 +0000

    auto commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    k = 0
     for i in range(x):
-        k = 0
         try:
             print("{:d}".format(my_list[i]), end="")
             k += 1
