495a7852e08da16d27edf85963d343b8db1207ce
Me <me@example.com>
2025-12-07 14:44:25 +0000

    auto commit

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+def list_division(my_list_1, my_list_2, list_length):
+    res = []
+    for i in range(list_length):
+        flag = True
+        try:
+            res.append(my_list_1[i] / my_list_2[i])
+            flag = False
+        except TypeError:
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        except IndexError:
+            print("out of range")
+        finally:
+            if flag:
+                res.append(0)
+    return res
+
