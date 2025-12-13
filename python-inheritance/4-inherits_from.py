a5fafd446711d91ba8e8855fe92d3c1fcd655993
Me <me@example.com>
2025-12-13 08:30:53 +0000

    auto commit

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
 Module: 3-is_kind_of_class
-A module defining a single function to check for class instance.
+A module defining a single function to check for subclass instance.
 
 This module is part of the Python - Inheritance project.
 """
@@ -9,8 +9,8 @@ This module is part of the Python - Inheritance project.
 
 def inherits_from(obj, a_class):
     """
-    Checks if an object is an instance of the specified
-    class or any of its subclasses.
+    Checks if an object is an instance of any subclass of the specified
+    class.
 
     Args:
         obj (any): The object to check.
@@ -18,7 +18,7 @@ def inherits_from(obj, a_class):
 
     Returns:
         bool: True if 'obj' is an instance of
-        'a_class' or any of subclasses of 'a_class',
+        any of subclasses of 'a_class',
         False otherwise.
     """
-    return isinstance(obj, a_class)
+    return isinstance(obj, a_class) and type(obj) is not a_class
