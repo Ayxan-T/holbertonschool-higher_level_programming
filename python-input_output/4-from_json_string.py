3f529cb63f3adedd0f406f7f970542cf9c37555b
Me <me@example.com>
2025-12-14 10:40:59 +0000

    auto commit

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+    Module: 4-from_json_string
+    This module is a part of the 'Python - Input/Output' project.
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+        A function that returns an object (Python data structure)
+        represented by a JSON string.
+        args:
+            my_str - the string to be deserialized 
+        returns:
+            pthon object
+    """
+
+    return json.loads(my_str)
