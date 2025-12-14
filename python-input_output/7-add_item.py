b73a4e62fbecd43c4d93d3bf8c46bfeb6d1b0667
Me <me@example.com>
2025-12-14 18:51:59 +0000

    auto commit

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,7 +4,8 @@
     This module is a part of the 'Python - Input/Output' project.
 """
 
-import os, sys
+import os
+import sys
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
