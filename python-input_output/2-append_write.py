a30178c2adc510feaa9fcebfbec7b83fc19fa6a9
Me <me@example.com>
2025-12-14 10:12:10 +0000

    auto commit

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Module: 2-append_write 
+    This module is a part of the 'Python - Input/Output' project.
+"""
+
+
+def append_write(filename="", text=""):
+    """
+        A function that appends a string at the end of a text file (UTF8)
+        and returns the number of characters added.
+        args:
+            filename - name of the file
+            text - the string to be appended 
+        returns:
+            number of characters appended
+    """
+
+    with open(filename, "a") as f:
+        return f.write(text)
