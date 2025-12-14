4af90bf8bd8b39e8773f07ac8b24016376af000e
Me <me@example.com>
2025-12-14 09:48:06 +0000

    auto commit

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+    Module: 1-write_file
+    This module is a part of the 'Python - Input/Output' project.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+        A function that writes a string to a text file (UTF8)
+        and returns the number of characters written.
+        args:
+            filename - name of the file
+            text - the string to be written
+        returns:
+            number of characters written
+    """
+    
+    with open(filename, "w") as f:
+        return f.write(text)
