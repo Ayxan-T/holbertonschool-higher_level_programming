5075041a4c0a96394861860101141ca65396bae5
Me <me@example.com>
2025-12-14 19:05:34 +0000

    auto commit

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,20 @@
+#/usr/bin/python3
+"""
+    Module: 8-class_to_json
+    This module is a part of the 'Python - Input/Output' project.
+"""
+import json
+
+
+def class_to_json(obj):
+    """ A  function that returns the dictionary description
+    with simple data structure (list, dictionary, string, integer
+    and boolean) for JSON serialization of an object.
+
+    args:
+        obj -- an instance of a class
+    returns:
+        serialized dictionary form of obj
+    """
+
+    return json.dumps(obj)
