05fe1e59e92963b5d62cc132f38bd56c3e0d6ce8
Me <me@example.com>
2025-12-17 12:52:24 +0000

    auto commit

---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Module: task_01_pickle
+    This module is a part of the 'Python - Serialization' project.
+"""
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def display(self):
+        print("""Name: {}
+        Age: {}
+        Is Student: {}""".format(self.name, self.age, self.is_student))
+
+    def serialize(self, filename):
+        pickled_obj = pickle.dumps(self)
+        with open(filename, "wb") as f:
+            f.write(pickled_obj)
+
+    @classmethod
+    def deserialize(cls, filename):
+        with open(filename, "rb") as f:
+            pickled_obj = f.read()
+        return pickle.loads(pickled_obj)
+
