8c5475895b50e9aa04309f2887bdd44a77815095
Me <me@example.com>
2025-12-21 12:32:57 +0000

    auto commit

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,7 @@
 -- listing privileges for users 'user_0d_1' and 'user_0d_2'
-SHOW GRANTS FOR `user_0d_1`@`localhost`
-UNION
-SHOW GRANTS FOR `user_0d_2`@`localhost`;
+-- List grants for user_0d_1
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- List grants for user_0d_2
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
+
