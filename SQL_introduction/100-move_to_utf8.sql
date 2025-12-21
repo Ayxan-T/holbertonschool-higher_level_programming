48ec17a444521b1cc7c204fcc10408a7c0ecd442
Me <me@example.com>
2025-12-21 08:19:30 +0000

    auto commit

--- SQL_introduction/100-move_to_utf8.sql
+++ SQL_introduction/100-move_to_utf8.sql
@@ -5,11 +5,11 @@ COLLATE utf8mb4_unicode_ci;
 
 -- 2. Convert the table and all its character columns
 ALTER TABLE hbtn_0c_0.first_table 
+CONVERT TO CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
 -- 3. Specifically convert the 'name' field
 ALTER TABLE hbtn_0c_0.first_table 
 MODIFY name VARCHAR(256) 
-CHARACTER SET utf8mb4 
 COLLATE utf8mb4_unicode_ci;
 
